ccd6c333b9e48ee456038bc445fba283b95bc20b
LastFlyingPig <serafael@mail.ru>
2017-07-01 06:13:26 +0300

    Please work!

--- bot.py
+++ bot.py
@@ -14,21 +14,27 @@ server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+    bot.reply_to(message, 'Cheer, ' + message.from_user.first_name)
+    
+@bot.message_handler(commands=['roll'])
+def roll(message):
+    bot.send_message(message.chat.id, str(randint(1, 6)))
+    
+@bot.message_handler(commands=['rolldice'])
+def rolldice(message):
+    rand_val = randint(1, 6)
+    dise_text = dices.dice_lib[rand_val]
+    bot.send_message(message.chat.id, dise_text)
+    
+@bot.message_handler(commands=['rollsticker'])
+def rollsticker(message):
+    rand_val = randint(1, 6)
+    sticker_id = dices.dice_id_lib[rand_val]
+    bot.send_sticker(message.chat.id, sticker_id)
     
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    if message.text == '/roll':
-        bot.send_message(message.chat.id, str(randint(1, 6)))
-    if message.text == '/rolldice':
-        rand_val = randint(1, 6)
-        dise_text = dices.dice_lib[rand_val]
-        bot.send_message(message.chat.id, dise_text)
-    if message.text == '/rollsticker':
-        rand_val = randint(1, 6)
-        sticker_id = dices.dice_id_lib[rand_val]
-        bot.send_sticker(message.chat.id, sticker_id)
-    elif message.text == '/src':
+    if message.text == '/src':
         bot.send_message(message.chat.id, src)
         
 @server.route(SECRET, methods=['POST'])
