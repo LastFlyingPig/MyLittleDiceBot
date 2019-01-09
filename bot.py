8604316357848d5ae8b36948f7e88503cf2726ca
Tammsaar Serafim <serafim.tammsaar@spbpu.com>
2019-01-09 19:41:33 +0300

    test

--- bot.py
+++ bot.py
@@ -55,7 +55,7 @@ def roll(message):
 @bot.message_handler(commands=['testThis'])
 def roll(message):  
     bot.send_message(message.chat.id, message.text)
-    msgSplit = message.text.replace('/testThis','').lstrip(' ').split(" ", 1)
+    msgSplit = message.text.replace('/testThis','').lstrip(' ').split(" ", 2)
     bot.send_message(message.chat.id, len(msgSplit))
     if len(msgSplit) == 2:
         chatId = msgSplit[0]
